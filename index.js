1a22b7485cd71bc90ed7d7c08f0962bd743ecafa
hendrymei25 <70930111+hendrymei25@users.noreply.github.com>
2022-06-05 19:59:18 +0700

    challenge 09

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (data, sortby) => {
+  sortby(data);
+  let temp = []
+  for (let i = 0; i < data.length; i++) {
+    temp.push(`${i + 1 }. ${data[i]}`)
+ }
+ return temp;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => {
+  return data.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) => {
+  return data.reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
